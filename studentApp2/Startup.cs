b1278f33121c52cb756c34cffbb4ce2d5fb5b202
Stefan Vraciu <vraciustefancatalin@gmail.com>
2020-05-25 15:24:34 +0300

    Initial project
    First commit

---
+++ studentApp2/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(studentApp2.Startup))]
+namespace studentApp2
+{
+    public partial class Startup
+    {
+        private object services;
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
