fe777accb93c95b4489c669161554ea16903bce9
Sebastian Neagrau <seba.ng@yahoo.com>
2020-06-01 04:22:57 +0300

    updated catalog

--- studentApp2/Models/Catalog.cs
+++ studentApp2/Models/Catalog.cs
@@ -12,27 +12,11 @@ namespace studentApp2.Models
         public int GradeID { get; set; }
         public int StudentID { get; set; }
         public int CourseID { get; set; }
+  
         public int Grade { get; set; }
         public DateTime? GradeDate { get; set; }
     }
 
-    public class CatalogTeacherViewModel
-    {
-
-        public List<UserStudentViewModel> Students { get; set; }
-        public int GradeID { get; set; }
-        public int Grade { get; set; }
-        public int CourseID { get; set; }
-        public string CourseName { get; set; }
-
-        public IEnumerable<SelectListItem> gradeList = new SelectList(Enumerable.Range(1, 10).
-                   Select(grade => new SelectListItem { Text = grade.ToString(), Value = grade.ToString() }));
-    }
-
-    public class CatalogStudentViewModel
-    {
-        public List<GradeStudentViewModel> grades { get; set; }
-    }
 
     public class GradeStudentViewModel
     {
